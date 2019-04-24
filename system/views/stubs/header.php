13bdc56ec06edef32f14f591aad0e936297d0375
jfkinslow <killajk2201@gmail.com>
2019-04-23 22:17:28 -0500

    Made the readme: Explain how to install the framework and Explain how to add a new page to it.

--- system/views/stubs/header.php
+++ system/views/stubs/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Joshua F. Kinslow <?php echo $title; ?></title>
+    <title>Joshua F. Kinslow - <?php echo $title; ?></title>
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
